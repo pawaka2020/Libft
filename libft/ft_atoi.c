c6217226e256120da67e9a2d8334bbd4ab36caea
Rinor Ramli <riramli@u91z01s05.42kl.edu.my>
2021-08-09 23:44:39 +0800

    from guaca

---
+++ libft/ft_atoi.c
@@ -0,0 +1,52 @@
+#include "libft.h"
+/*
+returns integer from string (CHECKED)
+*/
+static int	empty(char c)
+{
+	if (c == 32 || c == '\t' || c == '\n')
+		return (1);
+	else if (c == '\r' || c == '\v' || c == '\f')
+		return (1);
+	else
+		return (0);
+}
+
+static int	isdigit2(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	long	i;
+	long	nbr;
+	int		isneg;
+
+	i = 0;
+	nbr = 0;
+	isneg = 0;
+	while (str[i] && empty(str[i]))
+		i++;
+	if (str[i] && str[i] == '-')
+	{
+		isneg = 1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && isdigit2(str[i]))
+		nbr = (nbr * 10) + (str[i++] - '0');
+	if (isneg == 1)
+		return (-nbr);
+	return (nbr);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char *str = "    12345abc";
+	printf("atoi value = %d\n", atoi(str));
+	printf("ft_atoi value = %d\n", ft_atoi(str));
+}
+*/
---
+++ libft/ft_memmove.c
@@ -0,0 +1,40 @@
+/*
+copies len bytes from string src to string dst, non-destructive manner.
+
+RETURN VALUES
+     The memmove() function returns the original value of dst.
+*/
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*d;
+	char	*s;
+	size_t	i;
+
+	d = (char *)dst;
+	s = (char *)src;
+	i = 0;
+	if (d > s)
+		while (len-- > 0)
+			d[len] = s[len];
+	else
+	{
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+/*
+#include <stdio.h>
+int main () {
+	char str[5] = "abcde";
+	printf("str+2 = %s\n", str+2);
+	char *str2 = memmove(str+2, str, 3);
+	printf("After memcpy dest = %s\n", str);
+	printf("After memcpy, ft_memmove = %s\n", str2);
+}
+*/

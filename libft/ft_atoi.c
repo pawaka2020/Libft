bec392f633a399195f8c8d40f7b12cfe908a4330
Rinor Ramli <40174427+pawaka2020@users.noreply.github.com>
2021-08-10 05:28:00 +0000

    New change to ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,29 +1,29 @@
 #include "libft.h"
-//places index after all the empty spaces in str
-static void	adjustempty(const char *str, size_t *i)
-{
-	int	j;
 
-	j = -1;
-	while (j++, str[j])
-	{
-		if (str[j] == ' ' || str[j] == '\n' || str[j] == '\t' )
-			*i += 1;
-		else if(str[j] == '\v' || str[j] == '\f' || str[j] == '\r')
-			*i += 1;
-	}
+static int	emptyspace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t') 
+		return (1);
+	if (c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	else
+		return (0);
 }
-//places index after all the '-' and '+' letters in str
-//adjusts sign as well
-static void adjustsign(const char *str, size_t *i, int *sign)
+
+static void adjustempty(const char *str, size_t *i)
+{
+	while (emptyspace(str[*i]))
+		*i += 1;
+}
+
+static void setsign(const char *str, size_t *i, int *sign)
 {
 	if (str[*i] == '-')
 		*sign = -1;
 	if ((str[*i] == '-') || (str[*i] == '+'))
 		*i += 1;
 }
-//set nbr as long instead of int
-//for testing if nbr exceeds int max/min value
+
 int	ft_atoi(const char *str)
 {
 	long	nbr;
@@ -34,8 +34,8 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	i = 0;
 	adjustempty(str, &i);
-	adjustsign(str, &i, &sign);
-	while ((str[i] >= '0') && (str[i] <= '9'))
+	setsign(str, &i, &sign);
+	while (('0' <= str[i]) && (str[i] <= '9'))
 	{
 		nbr = (nbr * 10) + (str[i] - '0');
 		if (nbr > 2147483647 && sign == 1)
@@ -44,17 +44,20 @@ int	ft_atoi(const char *str)
 			return (0);
 		i++;
 	}
-	return (nbr * sign);
+	return (sign * nbr);
 }
 /*
 #include <stdio.h>
+void test(char *c)
+{
+	printf("atoi result for %s = %d\n", c, atoi(c));
+	printf("ft_atoi result for %s = %d\n\n", c, ft_atoi(c));
+}
+
 int	main(void)
 {
-	printf("for max long, atoi value = %d\n", atoi("99999999999999999999999999"));
-	printf("for max long, ft_atoi value = %d\n", ft_atoi("99999999999999999999999999"));
-	printf("for min long, atoi value = %d\n", atoi("-99999999999999999999999999"));
-	printf("for min long, ft_atoi value = %d\n", ft_atoi("-99999999999999999999999999"));
-	printf("for null, atoi value = %d\n", atoi("\0"));
-	printf("for null, ft_atoi value = %d\n", ft_atoi("\0"));
+	test("99999999999999999999999999");
+	test("-99999999999999999999999999");
+	test("-123THERE IS A NYANCAT UNDER YOUR BED");
 }
 */
